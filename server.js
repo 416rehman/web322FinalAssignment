0468a7482d203102a69458c1c59762dcaf55b48d
rehman <hayaturehmanahmadzai@gmail.com>
2021-07-09 14:58:39 -0400

    added declaration

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+/*********************************************************************************
+ * WEB322 – Assignment 03
+ * I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part
+ * of this assignment has been copied manually or electronically from any other source
+ * (including 3rd party web sites) or distributed to other students.
+ *
+ * Name: Hayaturehman Ahmadzai Student ID: 122539166_____ Date: 2021/07/09
+ *
+ * Online (Heroku) Link: https://a3ahmadzai.herokuapp.com/
+ *
+ ********************************************************************************/
 const express = require("express");
 const path = require("path");
 const data = require("./data-service.js");
